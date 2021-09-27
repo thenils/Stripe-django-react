765329c8783090984362d0addd6b927b2e2cf50c
thenils3 <nilesh.tutorbin@gmail.com>
2021-09-27 16:26:36 +0530

    first commit

---
+++ Frontend/stripe-app-front/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {
+    Elements,
+} from '@stripe/react-stripe-js';
+
+import { loadStripe } from "@stripe/stripe-js/pure";
+import CheckoutForm from "./components/CheckoutForm";
+
+const stripePromise = loadStripe('pk_test_51J5RXzSIyKBcYOPsQ2H6cqLwes6RoffzLAys9V7c0daeCOb4Q2MtNLct4EQEQQY3f8IXzvZD1PxLbvqpSspUeAFn00uALQbf2v');
+
+
+const App = () => ( <
+    Elements stripe = { stripePromise } >
+    <
+    CheckoutForm / >
+    <
+    /Elements>
+);
+
+export default App;
